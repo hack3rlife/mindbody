b73b2b2fc2bd753f6dea10bd3811e8af6af51f59
hack3rlife <hack3rlife@outlook.com>
2017-01-16 17:45:36 -0800

    1. Adding data-driven test style 2. Adding bug report 3. Adding test cases

---
+++ ParkingCalculatorUnitTest/ParkingCalculatorUnitTest/DataDrivenTesttCases.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingCalculatorAutomation;
+
+namespace ParkingCalculatorUnitTest
+{
+    [TestClass]
+    public class DataDrivenTesttCases : ParkingCalculatorTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ParkingPage.GoTo();
+        }
+
+        [TestMethod()]
+        [TestCategory("SmokeTest")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data\\input_values.csv", "input_values#csv", DataAccessMethod.Sequential)]
+        public void ParkingCalculator_Default_TestCases_NoError()
+        {
+            // arrange
+            var lot = InputParserHelper.GetParkingLotType(TestContext.DataRow[0].ToString());
+            var entryTime = InputParserHelper.GetDate(TestContext.DataRow[1].ToString());
+            var exitTime = InputParserHelper.GetDate(TestContext.DataRow[2].ToString());
+            var expectedCost = TestContext.DataRow[3];
+            var expectedDuration = TestContext.DataRow[4];
+
+            // act
+            ParkingPage.Create()
+                .WithParkingLot(lot)
+                .WithStartDateAndTime(entryTime)
+                .WithEndDateAndTime(exitTime)
+                .Calculate();
+
+            // assert
+            Assert.AreEqual(expectedCost, ParkingPageCost.Total, "The cost is different");
+            Assert.AreEqual(expectedDuration, ParkingPageCost.Description, "Datetime is incorrect");
+        }
+        
+    }
+}
---
+++ ParkingCalculatorUnitTest/ParkingCalculatorUnitTest/InputParserHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingCalculatorAutomation;
+
+namespace ParkingCalculatorUnitTest
+{
+    public static class InputParserHelper
+    {
+        public static ParkingLotType GetParkingLotType(string type)
+        {
+            switch (type)
+            {
+                case "EP":
+                    return ParkingLotType.EP;
+                case "LTG":
+                    return ParkingLotType.LTG;
+                case "LTS":
+                    return ParkingLotType.LTS;
+                case "STP":
+                    return ParkingLotType.STP;
+                case "VP":
+                    return ParkingLotType.VP;
+                default:
+                    throw new InvalidCastException(nameof(type));
+            }
+        }
+
+        public static DateTime GetDate(string date)
+        {
+            DateTime dt;
+
+            if (DateTime.TryParse(date, out dt))
+                return dt;
+
+            throw new InvalidOperationException(nameof(date));
+        }
+    }
+}
