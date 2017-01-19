e4dda358022d3bea8cc0ff13feaf92403504daa7
hack3rlife <hack3rlife@outlook.com>
2017-01-19 11:45:09 -0800

    1. Adding input_values.csv files a deploy item just in case this could be an issue when running the tests in another location. 2. Updating NuGet packages

--- ParkingCalculatorUnitTest/ParkingCalculatorUnitTest/DataDrivenTestCases.cs
+++ ParkingCalculatorUnitTest/ParkingCalculatorUnitTest/DataDrivenTestCases.cs
@@ -5,6 +5,7 @@ using ParkingCalculatorAutomation;
 namespace ParkingCalculatorUnitTest
 {
     [TestClass]
+    [DeploymentItem("data\\input_values.csv")]
     public class DataDrivenTestCases : ParkingCalculatorTest
     {
         public TestContext TestContext { get; set; }
@@ -20,8 +21,8 @@ namespace ParkingCalculatorUnitTest
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data\\input_values.csv", "input_values#csv", DataAccessMethod.Sequential)]
         public void ParkingCalculator_Default_TestCases_NoError()
         {
-            // arrange
-            var lot = InputParserHelper.GetParkingLotType(TestContext.DataRow[0].ToString());
+           // arrange
+           var lot = InputParserHelper.GetParkingLotType(TestContext.DataRow[0].ToString());
             var entryTime = InputParserHelper.GetDate(TestContext.DataRow[1].ToString());
             var exitTime = InputParserHelper.GetDate(TestContext.DataRow[2].ToString());
             var expectedCost = TestContext.DataRow[3];
@@ -35,7 +36,7 @@ namespace ParkingCalculatorUnitTest
                 .Calculate();
 
             this.TakeScreenShot(this.TestContext.TestName + DateTime.Now.Ticks);
-            
+
             // assert
             Assert.AreEqual(expectedCost, ParkingPage.Total, "The cost is different");
             Assert.AreEqual(expectedDuration, ParkingPage.Description, "Datetime is incorrect");
